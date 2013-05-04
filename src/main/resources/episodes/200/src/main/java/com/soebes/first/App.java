3aee26cb3d737c36a24cb0a3bb83d6ee6375c4a7
Karl Heinz Marbaise <info@soebes.de>
2013-05-04 18:39:22 +0200

    - Added new episode for maven-jar-plugin.

---
+++ src/main/resources/episodes/200/src/main/java/com/soebes/first/App.java
@@ -0,0 +1,13 @@
+package com.soebes.first;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
